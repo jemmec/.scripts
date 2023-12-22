7601b9bd3ea588a34fe6247316d25fe8b8f7644d
jemmec <jwmcdade96@gmail.com>
2023-12-22 13:22:18 +1030

    slight improvement

--- fz.bat
+++ fz.bat
@@ -9,4 +9,4 @@ if /I "%2"=="-a" set "isAll=true"
 set FZF_DEFAULT_COMMAND=fd --type d --type l --threads 20 --exclude node_modules --glob
 if %isAll%==true set "FZF_DEFAULT_COMMAND=%FZF_DEFAULT_COMMAND% --hidden"
 
-for /F "tokens=*" %%i in ('fzf') do %1 %%i
+for /F "tokens=*" %%i in ('fzf') do %1 %%i & cd %%i
